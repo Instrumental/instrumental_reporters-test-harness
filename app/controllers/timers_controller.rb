d8dc8ab7ffb4cd4e260a0d574a8da0304a31d38f
Joel Meador <joel@expectedbehavior.com>
2017-04-10 18:29:42 -0400

    add a controller that tests method tracing

---
+++ app/controllers/timers_controller.rb
@@ -0,0 +1,17 @@
+class TimersController < ApplicationController
+  def show
+  end
+
+  def create
+    do_timer
+    redirect_to timers_path
+  end
+
+  private
+
+  def do_timer
+    value = [[0, params[:value].to_i].min, 5].max
+    sleep(rand * value)
+  end
+  add_instrumental_method_tracer :do_timer
+end
