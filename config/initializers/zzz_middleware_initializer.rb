a98532e54d066e450cc55e0260bce1ec24fe7cf8
Joel Meador <joel@expectedbehavior.com>
2017-07-12 15:49:07 -0400

    branch with this middleware hasn't been merged to github master yet

--- config/initializers/zzz_middleware_initializer.rb
+++ config/initializers/zzz_middleware_initializer.rb
@@ -1,2 +1,2 @@
-MetricianTestHarness::Application.config.middleware.delete(Metrician::Middleware::RequestTiming)
-MetricianTestHarness::Application.config.middleware.insert(0, Metrician::Middleware::RequestTiming)
+MetricianTestHarness::Application.config.middleware.delete(Metrician::RequestTiming)
+MetricianTestHarness::Application.config.middleware.insert(0, Metrician::RequestTiming)
