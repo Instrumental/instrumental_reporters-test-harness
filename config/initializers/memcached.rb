2322ea086853cbf1415fc6753f277987a0328bd6
Joel Meador <joel@expectedbehavior.com>
2017-07-14 17:37:33 -0400

    try to fix memcached connection settings

--- config/initializers/memcached.rb
+++ config/initializers/memcached.rb
@@ -1,1 +1,6 @@
-$memcached = Dalli::Client.new("localhost:11211")
+$memcached = Dalli::Client.new(ENV["MEMCACHEDCLOUD_SERVERS"] || "localhost:11211")
+# if Rails.env.production?
+#   $memcached = Dalli::Client.new
+# else
+#   $memcached = Dalli::Client.new("localhost:11211")
+# end
